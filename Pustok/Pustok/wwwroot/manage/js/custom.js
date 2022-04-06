ea9e2f3a0c04bab1e7428864c0f998d1b6b6ed83
HamidMammadov <hamidvm@code.edu.az>
2022-04-06 17:50:28 +0400

    First Commit

---
+++ Pustok/Pustok/wwwroot/manage/js/custom.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#deletefilesweet", function (e) {
+        e.preventDefault();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success',
+
+                    fetch($(this).attr("href")).then(res =>
+                    {
+                        //if (res.ok) {
+                        //    alert("Silindi")
+                        //}
+
+                        return res.text();
+                    }).then(data => {
+                        $("#ProductTable").html(data)
+                    })
+                )
+            }
+        })
+    })
+})
